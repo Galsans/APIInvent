b24099f277aba5c9407b239fc7d7991acb4175ec
UndanganDigitalDarmaAnnes <d.sanjayaaditama@gmail.com>
2024-07-23 11:54:49 +0700

    Authentikasi Api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        if ($validate->fails()) {
+            return response()->json([
+                'msg' => $validate->errors()
+            ], 422);
+        }
+        $input = $request->all();
+        $input['password'] = Hash::make($request->password);
+        $user = User::create($input);
+
+        return response()->json([
+            'msg' => 'berhasil membuat akun',
+            'akun' => $user,
+            'token' => $user->createToken('apiToken')->plainTextToken
+        ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        if ($validate->fails()) {
+            return response()->json([
+                'msg' => $validate->errors()
+            ], 422);
+        }
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'msg' => 'credential salah'
+            ], 422);
+        }
+
+        return response()->json([
+            'msg' => 'berhasil login',
+            'akun' => $user,
+            'token' => $user->createToken('apiToken')->plainTextToken
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'msg' => 'berhasil logout'
+        ], 200);
+    }
+}
